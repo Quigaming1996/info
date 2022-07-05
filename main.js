4a645236a4ec57a4d3d848e4618d45302e219a5c
Quigaming1996 <42017060+Quigaming1996@users.noreply.github.com>
2022-07-05 19:02:05 +0700

    profile

---
+++ main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal ({
+    distance: '45px',
+    duration: 1500,
+    reset: true
+})
+
+sr.reveal('.content', {delay: 50, origin:'left'})
+sr.reveal('.about-text,.img2', {delay: 0, origin:'right'})
+sr.reveal('.w-container', {delay: 0, origin:'right'})
+sr.reveal('.let-me', {delay: 0, origin:'right'})
+sr.reveal('.footer-d,.footer-p', {delay: 0, origin:'right'})
+
+
+
