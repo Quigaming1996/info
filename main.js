1c444152b0a3485fd5f22c8e65b917282e471890
Quigaming1996 <42017060+Quigaming1996@users.noreply.github.com>
2022-07-12 16:18:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const sr = ScrollReveal ({
 
 sr.reveal('.content', {delay: 50, origin:'left'})
 sr.reveal('.about-text,.img2', {delay: 0, origin:'right'})
-sr.reveal('.w-container', {delay: 0, origin:'right'})
+sr.reveal('.w-container', {delay: 0, origin:'left'})
 sr.reveal('.let-me', {delay: 0, origin:'right'})
 sr.reveal('.footer-d,.footer-p', {delay: 0, origin:'right'})
 
